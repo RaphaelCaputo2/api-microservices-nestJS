78dfc360f1d9a09d6b2492873a48c5ee758d28d8
RaphaelCaputo2 <faelelf2@gmail.com>
2022-01-05 00:31:40 -0300

    Adicionando DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
+const { URL_DB } = process.env;
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(URL_DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
